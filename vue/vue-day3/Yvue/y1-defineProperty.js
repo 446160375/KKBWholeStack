35671af008cb33b4c6fb7e4e7c9b37574aaf3f9d
殷兴平 <446160375@qq.com>
2020-08-07 16:51:58 +0800

    vueday3

---
+++ vue/vue-day3/Yvue/y1-defineProperty.js
@@ -0,0 +1,51 @@
+// object.defineProperty()
+// 拦截：对某个对象的某个key做拦截
+function defineReactive(obj, key, val) {
+    // 如果val是对象，需要递归处理之
+    observe(val)
+
+    Object.defineProperty(obj, key, {
+        get() {
+            console.log('get:', key);
+            return val
+        },
+        set(newVal) {
+            if (val !== newVal) {
+                // 如果newVal是对象，也要做响应式处理
+                observe(newVal)
+                val = newVal
+                console.log('set:', key, newVal)
+            }
+        }
+    })
+}
+
+// 遍历指定数据对象每个key,拦截他们
+function observe(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return obj
+    }
+    // console.log('ArrKey:',Object.keys(obj))
+    Object.keys(obj).forEach(key => {
+        defineReactive(obj, key, obj[key])
+    })
+}
+
+function set(obj, key, val) {
+    defineReactive(obj, key, val)
+}
+
+const obj = {
+    foo: 'foo',
+    bar: 'bar',
+    baz: {
+        a: 1
+    }
+}
+
+observe(obj)
+// obj.foo
+// obj.foo = 'foooooooo'
+
+set(obj, 'dong', 'dong')
+obj.dong = 'dongdong'
